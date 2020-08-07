0f423f0279a07cc27b08d99f3482b08630c1058b
Natalya Storchilova <nata@Storcilovas-MacBook-Pro.local>
2020-08-08 01:54:16 +0300

    Added PostManager.java

---
+++ src/main/java/ru/netology/manager/PostManager.java
@@ -0,0 +1,24 @@
+package ru.netology.manager;
+
+import ru.netology.domain.Post;
+
+public class PostManager {
+    private Post[] posts;                      // с􏰀юда можно класт􏰆ь л􏰀юбых менеджеров
+
+    /**
+     * Main Page generation
+     */
+
+    public Post[] search() {      // но метод у каждого свой (со своей логикой)
+        for (Post post : posts) {
+            // TODO: add logic
+            return null;
+        }
+    }
+
+    public void delete() {
+        for (Post post : posts) {
+            // TODO: add logic
+        }
+    }
+}
